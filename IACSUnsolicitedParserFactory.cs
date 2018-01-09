4dd6109eb17d36ea91cef328e126d55d8780056e
Brad Foster <bfoster@coredial.com>
2018-01-09 15:52:20 -0600

    delete everything and start over

--- IACSUnsolicitedParserFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿ // ReSharper disable once CheckNamespace
-namespace TSAPIClient.Parsers
-{
-    public interface IACSUnsolicitedParserFactory
-    {
-        IACSUnsolicitedParser CreateParser (int eventType);
-    }
-}
