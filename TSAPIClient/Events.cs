b219ae3424da151ee753e43f24fd7885046f6199
Brad Foster <brad@voice4net.com>
2018-01-23 08:44:31 -0600

    add missing file Events.cs (thanks taniaminter)

---
+++ TSAPIClient/Events.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TSAPIClient.ATT;
+using TSAPIClient.CSTA;
+
+namespace TSAPIClient
+{
+    public class TSAPIEventArgs : EventArgs
+    {
+        public CSTAEvent_t cstaEvent { get; set; }
+
+        public ATTEvent_t attEvent { get; set; }
+
+        public PrivateData_t? privateData { get; set; }
+
+        private string m_SequenceNumber = string.Empty;
+        public string SequenceNumber
+        {
+            get { return m_SequenceNumber; }
+            set { m_SequenceNumber = value; }
+        }
+    }
+}
